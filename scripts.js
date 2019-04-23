0221cccb6b9ff5a8830112f43e3aa2f924df5653
Jacob <chubbi1968@gmail.com>
2019-04-23 22:08:34 +0200

    script

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(
 	const url = 'https://api.scryfall.com/cards/random?format=image&version=art_crop';
 	$.get(url,function(data,status){
-		console.log($(data));
-		console.log($(status));
-	});	
+		console.log(data);
+		console.log(status);
+	});
 );
