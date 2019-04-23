349bec6b5d812b5202fc7cd6c7e825ff69865cc2
Jacob <chubbi1968@gmail.com>
2019-04-23 22:10:51 +0200

    script

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
+const url = 'https://api.scryfall.com/cards/random?format=image&version=art_crop';
+
 jQuery(document).ready(
-	const url = 'https://api.scryfall.com/cards/random?format=image&version=art_crop';
 	$.get(url,function(data,status){
 		console.log(data);
 		console.log(status);
