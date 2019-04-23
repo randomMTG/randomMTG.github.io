906c3384b34bd6776adb64b793e36a30aa09cf0b
Jacob <chubbi1968@gmail.com>
2019-04-23 23:06:27 +0200

    Redirect url to img

--- scripts.js
+++ scripts.js
@@ -1,23 +1,7 @@
 jQuery(document).ready( function(){
 	const url = 'https://api.scryfall.com/cards/random?format=image&version=art_crop';
-	// $.get(url,function(data,status){
-	// 	console.log(`${data}`)
-	// 	// console.log(`${status}`)
-	// });
-
-	var STATUS = {
-		REDIRECT: 280
-	};
-
-	$.get(url, function(data, status, dataType) {
-		console.log(JSON.stringify(data, null, 4));
-		console.log(`${status}`);
-		console.log(`${dataType}`);
-		// $("#mtg").attr('src',${data});
-	});
 
 	var xhr = new XMLHttpRequest();
-
 	$.ajax({
 		url: url,
 		type: 'get',
@@ -26,6 +10,7 @@ jQuery(document).ready( function(){
 		},
 		success: function () {
 			console.log(xhr.responseURL);
+			$("#mtg").attr('src',xhr.responseURL);
 		}
 	});
 
