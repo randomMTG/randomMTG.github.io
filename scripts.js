972c4ad6ac518111a92b42f2e36f7d21d3cebe6c
Jacob <chubbi1968@gmail.com>
2019-04-23 21:08:33 +0200

    clean

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-// Load navbar
 jQuery(document).ready(
-	function ($){
-		window.location.href = 'https://api.scryfall.com/cards/random?format=image&version=art_crop'
-	}
+	window.location.href = 'https://api.scryfall.com/cards/random?format=image&version=art_crop'
 );
