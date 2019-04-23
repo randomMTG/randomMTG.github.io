a776814a0da7e39db35a665d27bda58777ba4251
Jacob <chubbi1968@gmail.com>
2019-04-23 22:42:31 +0200

    test

--- scripts.js
+++ scripts.js
@@ -1,35 +1,35 @@
 jQuery(document).ready( function(){
 	const url = 'https://api.scryfall.com/cards/random?format=image&version=art_crop';
-	$.get(url,function(data,status){
-		console.log(`${data}`)
-		// console.log(`${status}`)
-	});
+	// $.get(url,function(data,status){
+	// 	console.log(`${data}`)
+	// 	// console.log(`${status}`)
+	// });
 
 	var STATUS = {
 		REDIRECT: 280
 	};
 
-	$.post('/redirected', {}, function(response, status, request) {
+	$.post(url, {}, function(response, status, request) {
 		if (status == STATUS.REDIRECT) {
 			// you need to return the redirect url
 			console.log(response.redirectUrl);
 		} else {
-			$('#content').html(request.responseText);
+			console.log(request.responseText);
 		}
 	});
 
-	var xhttp = new XMLHttpRequest();
-
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			console.log(this.responseText);
-			console.log(this.getAllResponseHeaders());
-			console.log(this.responseXML);
-		}
-	};
-
-	xhttp.open("GET", url, true);
-	xhttp.send();
+	// var xhttp = new XMLHttpRequest();
+	//
+	// xhttp.onreadystatechange = function() {
+	// 	if (this.readyState == 4 && this.status == 200) {
+	// 		console.log(this.responseText);
+	// 		console.log(this.getAllResponseHeaders());
+	// 		console.log(this.responseXML);
+	// 	}
+	// };
+	//
+	// xhttp.open("GET", url, true);
+	// xhttp.send();
 
 	// xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	// xhttp.send("fname=Henry&lname=Ford");
