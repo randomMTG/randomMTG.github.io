09ce71c912d40e7056dd1da67d9738e2ced47e01
Jacob <chubbi1968@gmail.com>
2019-04-24 20:33:32 +0200

    test

--- scripts.js
+++ scripts.js
@@ -2,15 +2,22 @@ jQuery(document).ready( function(){
 	const url = 'https://api.scryfall.com/cards/random?format=image&version=art_crop';
 
 	var xhr = new XMLHttpRequest();
-	$.ajax({
-		url: url,
-		type: 'get',
-		xhr: function() {
-			return xhr;
-		},
-		success: function () {
-			console.log(xhr.responseURL);
-			$("#mtg").attr('src',xhr.responseURL);
-		}
-	});
+	xhr.open('GET', url, true);
+	xhr.onload = function () {
+		console.log(xhr.responseURL); // http://example.com/test
+	};
+	xhr.send(null);
+
+	// var xhr = new XMLHttpRequest();
+	// $.ajax({
+	// 	url: url,
+	// 	type: 'get',
+	// 	xhr: function() {
+	// 		return xhr;
+	// 	},
+	// 	success: function () {
+	// 		console.log(xhr.responseURL);
+	// 		$("#mtg").attr('src',xhr.responseURL);
+	// 	}
+	// });
 });
