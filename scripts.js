67cf4830e60609c4240efb959ccf8b72b138a7f9
Jacob <chubbi1968@gmail.com>
2019-04-29 16:07:29 +0200

    smarter loading

--- scripts.js
+++ scripts.js
@@ -1,25 +1,51 @@
 const url = 'https://api.scryfall.com/cards/random?format=image&version=art_crop';
 var xhr = new XMLHttpRequest();
+var load = "";
+var tmp = null;	// Variable to hold click event
 
 jQuery(document).ready( function(){
-
-	// Image on load
-	newMTG();
-
-	// Image on click
-	$("#mtg").click( newMTG );
-});
-
-function newMTG(){
-
-	// Load GIF
-	$("#mtg").attr('src',"pacman.gif");
 
 	// Request random MTG
 	xhr.open('GET', url, true);
 	xhr.onload = function () {
 		console.log(xhr.responseURL);
 		$("#mtg").attr('src',xhr.responseURL);
+	};
+	xhr.send(null);
+
+	// Load next image before click to avoid waiting
+	newMTG();
+
+	// Image on click
+	$("#mtg").click( function(){
+
+		if(xhr.readyState==4){
+			// If loaded, present and load another
+			$("#mtg").attr('src',load);
+			newMTG();
+		} else {
+			// Else wait for loading, present and load another
+			$("#mtg").attr('src',"pacman.gif"); // Loading GIF
+			tmp = $("#mtg").click;
+			$("#mtg").click = null;
+			xhr.onreadystatechange( function() {
+				if(xhr.readyState==4){
+					$("#mtg").attr('src',load);
+					newMTG();
+					$("#mtg").click = tmp;
+				}
+			});
+		}
+
+	});
+});
+
+// Request random MTG
+function newMTG(){
+	xhr.open('GET', url, true);
+	xhr.onload = function () {
+		console.log(xhr.responseURL);
+		load = xhr.responseURL;
 	};
 	xhr.send(null);
 }
