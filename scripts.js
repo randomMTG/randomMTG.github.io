7ac7dcd2c46b3171cb37d3d4bd2104300d5e985d
Jacob <chubbi1968@gmail.com>
2019-04-29 16:21:11 +0200

    initial load

--- scripts.js
+++ scripts.js
@@ -12,32 +12,33 @@ jQuery(document).ready( function(){
 		$("#mtg").attr('src',xhr.responseURL);
 		// Load next image before click to avoid waiting
 		newMTG();
+
+		// Image on click - available after loading first image
+		$("#mtg").click( function(){
+
+			if(xhr.readyState==4){
+				// If loaded, present and load another
+				$("#mtg").attr('src',load);
+				newMTG();
+			} else {
+				// Else wait for loading, present and load another
+				$("#mtg").attr('src',"pacman.gif"); // Loading GIF
+				tmp = $("#mtg").click;
+				$("#mtg").click = null;
+				xhr.onreadystatechange = function() {
+					if(xhr.readyState==4){
+						$("#mtg").attr('src',load);
+						$("#mtg").click = tmp;
+						xhr.onreadystatechange = null;
+						newMTG();
+					}
+				};
+			}
+
+		});
 	};
 	xhr.send(null);
 
-	// Image on click
-	$("#mtg").click( function(){
-
-		if(xhr.readyState==4){
-			// If loaded, present and load another
-			$("#mtg").attr('src',load);
-			newMTG();
-		} else {
-			// Else wait for loading, present and load another
-			$("#mtg").attr('src',"pacman.gif"); // Loading GIF
-			tmp = $("#mtg").click;
-			$("#mtg").click = null;
-			xhr.onreadystatechange = function() {
-				if(xhr.readyState==4){
-					$("#mtg").attr('src',load);
-					newMTG();
-					$("#mtg").click = tmp;
-					xhr.onreadystatechange = null;
-				}
-			};
-		}
-
-	});
 });
 
 // Request random MTG
