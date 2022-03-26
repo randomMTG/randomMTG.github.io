7195973846b1f25dc3a72c24bd2da58653dc9664
JACS <jacs@zbc.dk>
2022-03-26 17:33:10 +0100

    simplified js (using fetch etc)

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,42 @@
 const url = "https://api.scryfall.com/cards/random?format=image&version=art_crop";
-let xhr = new XMLHttpRequest();
-let load = "";
-let tmp = null; // Variable to hold click event
-let im = new Image(); // Image object to preload image
-im.src = "images/pacman.gif";
-const img_el = document.querySelector("#mtg");
+const img = document.querySelector("#mtg");
+const next = new Image();
+let loaded = false;
+let waiting = false;
 
-// Request random MTG
-xhr.open("GET", url, true);
-xhr.onload = function () {
-    console.log(xhr.responseURL);
-    img_el.src = xhr.responseURL;
+fetch(url).then((res) => {
+    img.src = res.url;
 
-    // Load next image before click to avoid waiting
-    newMTG();
+    get_next();
 
-    // Image on click - available after loading first image
-    img_el.onclick = () => {
-        console.log(xhr.readyState);
-
-        if (xhr.readyState == 4 && load != "") {
-            // If loaded, present and load another
-            img_el.src = load;
-            newMTG();
+    img.onclick = () => {
+        if (loaded) {
+            change_img();
+            get_next();
         } else {
-            // Else wait for loading, present and load another
-            img_el.src = "images/pacman.gif"; // Loading GIF
-            tmp = img_el.onclick;
-            img_el.onclick = null;
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4) {
-                    img_el.src = xhr.responseURL;
-                    img_el.onclick = tmp;
-                    xhr.onreadystatechange = null;
-                    newMTG();
-                }
-            };
+            img.src = "images/pacman.gif";
+            waiting = true;
         }
     };
-};
-xhr.send(null);
+});
 
-// Request random MTG
-function newMTG() {
-    xhr.open("GET", url, true);
-    xhr.onload = function () {
-        console.log(xhr.responseURL);
-        load = xhr.responseURL;
-        im.src = load;
-    };
-    xhr.send(null);
-}
+// Get second image.
+get_next = () => {
+    fetch(url).then((res) => {
+        next.src = res.url;
+        loaded = true;
+
+        // If already waiting for new image then display and get new image immediately
+        if (waiting) {
+            change_img();
+            waiting = false;
+            get_next();
+        }
+    });
+};
+
+// Change image to the next
+change_img = () => {
+    img.src = next.src;
+    loaded = false;
+};
